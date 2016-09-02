8786f80f077250e0df67dade27f88d0fdd37ec88
zmicer <dzmitrykashlach@gmail.com>
2016-09-02 16:31:21 +0300

    JEN-165:search-filter.js;search.js

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,27 +1,18 @@
-    $.expr[":"].containsNoCase = function (el, i, m) {
-        var search = m[3];
-        if (!search) return false;
-          return new RegExp(search,"i").test($(el).text());
-    };
+$("#search-box").keyup(function() {
+        var search = $(this).val();
+        $("#testId").show();
+        if (search)
+            $("#testId").not(":containsNoCase(" + search + ")").hide();
+});
 
-    $.fn.searchFilter = function (options) {
-        var opt = $.extend({
-            // target selector
-            targetSelector: "",
-            // number of characters before search is applied
-            charCount: 1
-        }, options);
+$.expr[":"].containsNoCase = function (el, i, m) {
+    var search = m[3];
+    if (!search) return false;
+      return new RegExp(search,"i").test($(el).text());
+};
 
-        return this.each(function () {
-            var $el = $(this);
-            $el.on("keyup",function () {
-                var search = $(this).val();
 
-                var $target = $(opt.targetSelector);
-                $target.show();
+// jQuery Plug-in example
+$("#testId")
+    .searchFilter({targetSelector: "#testId"})
 
-                if (search && search.length >= opt.charCount)
-                    $target.not(":containsNoCase(" + search + ")").hide();
-            });
-        });
-    };
